6d06a823b17a6385cd16cefac180966fa7d9ff05
Harry Bromley <harry.bromley@aegiq.com>
2024-05-23 10:17:38 +0100

    Removed old code in state utils

--- lightworks/sdk/utils/state_utils.py
+++ lightworks/sdk/utils/state_utils.py
@@ -15,36 +15,6 @@
 """
 Script to store various useful functions for the simulation aspect of the code.
 """
-
-def fock_basis(N: int, n: int, statistic_type: str) -> list:
-    """
-    Returns the Fock basis for n photons in N modes for either bosonic of
-    fermionic statistics.
-    """
-    if statistic_type == "bosonic":
-        return list(_sums(N,n))
-    elif statistic_type == "fermionic":
-        return _fermionic_basis(N, n)
-    else:
-        raise ValueError("statistic_type should be 'bosonic' or 'fermionic'.")
-    
-def _fermionic_basis(N: int, n: int) -> list:
-    """This returns the possible states of n fermions in N modes as vectors."""
-    if n == 0:
-        return [[0]*N]
-    if N == n:
-        return [[1]*N]
-    arrays_with_zero = [[0]+arr for arr in _fermionic_basis(N - 1, n)]
-    arrays_with_one = [[1]+arr for arr in _fermionic_basis(N - 1, n - 1)]
-    return arrays_with_zero + arrays_with_one
-
-def _sums(length: int, total_sum: int):
-    if length == 1:
-        yield [total_sum,]
-    else:
-        for value in range(total_sum + 1):
-            for permutation in _sums(length-1, total_sum-value):
-                yield permutation + [value,]
                 
 def state_to_string(state: list) -> str:
     """Converts the provided state to a string with ket notation."""
