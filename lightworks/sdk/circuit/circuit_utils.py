a15e264c61d31f2851a8804bd918a366a00cc696
Harry Bromley <harry.bromley@aegiq.com>
2024-06-21 11:03:04 +0100

    mypy issue fix

--- lightworks/sdk/circuit/circuit_utils.py
+++ lightworks/sdk/circuit/circuit_utils.py
@@ -307,7 +307,7 @@ def check_loss(loss: float | Parameter) -> bool:
         loss = loss.get()
     if not isinstance(loss, Number) or isinstance(loss, bool):
         raise TypeError("Loss value should be numerical or a Parameter.")
-    if loss < 0:
+    if loss < 0:  # type: ignore
         raise ValueError("Provided loss values should be greater than 0.")
     return True
 
