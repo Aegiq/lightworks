f2a29e5b01de47257c2297f9df3f0686f9d0782b
Harry Bromley <harry.bromley@aegiq.com>
2024-08-01 14:39:04 +0100

    Updated docstring and removed unused function

--- lightworks/sdk/circuit/circuit_utils.py
+++ lightworks/sdk/circuit/circuit_utils.py
@@ -20,7 +20,6 @@ Contains a number of different utility functions for modifying circuits.
 
 from copy import copy
 from numbers import Number
-from typing import Any
 
 from ..utils import add_mode_to_unitary
 from .components import (
@@ -322,10 +321,9 @@ def combine_mode_swap_dicts(swaps1: dict, swaps2: dict) -> dict:
     return {m1: m2 for m1, m2 in new_swaps.items() if m1 != m2}
 
 
-def check_loss(loss: float | Parameter) -> bool:
+def check_loss(loss: float | Parameter) -> None:
     """
-    Check that loss is positive and toggle _loss_included if not already
-    done.
+    Check that loss is assigned to a positive value.
     """
     if isinstance(loss, Parameter):
         loss = loss.get()
@@ -333,14 +331,4 @@ def check_loss(loss: float | Parameter) -> bool:
         raise TypeError("Loss value should be numerical or a Parameter.")
     if not 0 <= loss <= 1:  # type: ignore
         raise ValueError("Provided loss values should be in the range [0,1].")
-    return True
-
-
-def display_loss_check(loss: Any) -> bool:
-    """
-    Checks whether a loss element should be shown when using the display
-    function.
-    """
-    if isinstance(loss, str):
-        return True
-    return bool(loss > 0)
+    return
