cfe45c4e9d83658a3b1dc17079dad047fa701b43
Harry Bromley <harry.bromley@aegiq.com>
2024-08-23 10:28:36 +0100

    Switched some StateTomography methods to class methods + added unit tests for number of measurements

--- lightworks/tomography/state_tomography.py
+++ lightworks/tomography/state_tomography.py
@@ -151,12 +151,14 @@ class StateTomography:
                 process.
 
         """
-        req_measurements, result_mapping = self._get_required_measurements()
+        req_measurements, result_mapping = (
+            StateTomography._get_required_measurements(self.n_qubits)
+        )
 
         # Generate all circuits and run experiment
         circuits = [
             self._create_circuit(
-                [self._get_measurement_operator(g) for g in gates]
+                [StateTomography._get_measurement_operator(g) for g in gates]
             )
             for gates in req_measurements
         ]
@@ -168,10 +170,12 @@ class StateTomography:
         results_dict = dict(zip(req_measurements, all_results))
         results_dict = {
             c: results_dict[result_mapping[c]]
-            for c in self._get_all_measurements()
+            for c in StateTomography._get_all_measurements(self.n_qubits)
         }
 
-        self._rho = self._calculate_density_matrix(results_dict)
+        self._rho = StateTomography._calculate_density_matrix(
+            self.n_qubits, results_dict
+        )
         return self.rho
 
     def fidelity(self, rho_exp: np.ndarray) -> float:
@@ -189,37 +193,6 @@ class StateTomography:
 
         """
         return state_fidelity(self.rho, rho_exp)
-
-    def _calculate_density_matrix(self, results: dict[str, dict]) -> np.ndarray:
-        """
-        Calculates the density matrix using a provided dictionary of results
-        data. The keys of this dictionary should be the measurement basis and
-        the values the results.
-        """
-        # Process results to find density matrix
-        rho = np.zeros((2**self.n_qubits, 2**self.n_qubits), dtype=complex)
-        for comb, res in results.items():
-            gates = [*comb]
-            total = 0
-            n_counts = 0
-            for s, c in res.items():
-                n_counts += c
-                # Adjust multiplier to account for variation in eigenvalues
-                multiplier = 1
-                for j, gate in enumerate(gates):
-                    if gate == "I" or s[2 * j : 2 * j + 2] == State([1, 0]):
-                        multiplier *= 1
-                    elif s[2 * j : 2 * j + 2] == State([0, 1]):
-                        multiplier *= -1
-                total += multiplier * c
-            total /= (2**self.n_qubits) * n_counts
-            # Calculate tensor product of the operators used
-            mat = self._get_pauli_matrix(gates[0])
-            for g in gates[1:]:
-                mat = np.kron(mat, self._get_pauli_matrix(g))
-            # Updated density matrix
-            rho += total * mat
-        return rho
 
     def _create_circuit(self, measurement_operators: list) -> Circuit:
         """
@@ -240,30 +213,70 @@ class StateTomography:
 
         return circuit
 
-    def _get_all_measurements(self) -> list[str]:
+    @staticmethod
+    def _calculate_density_matrix(
+        n_qubits: int, results: dict[str, dict]
+    ) -> np.ndarray:
+        """
+        Calculates the density matrix using a provided dictionary of results
+        data. The keys of this dictionary should be the measurement basis and
+        the values the results.
+        """
+        # Process results to find density matrix
+        rho = np.zeros((2**n_qubits, 2**n_qubits), dtype=complex)
+        for comb, res in results.items():
+            gates = [*comb]
+            total = 0
+            n_counts = 0
+            for s, c in res.items():
+                n_counts += c
+                # Adjust multiplier to account for variation in eigenvalues
+                multiplier = 1
+                for j, gate in enumerate(gates):
+                    if gate == "I" or s[2 * j : 2 * j + 2] == State([1, 0]):
+                        multiplier *= 1
+                    elif s[2 * j : 2 * j + 2] == State([0, 1]):
+                        multiplier *= -1
+                total += multiplier * c
+            total /= (2**n_qubits) * n_counts
+            # Calculate tensor product of the operators used
+            mat = StateTomography._get_pauli_matrix(gates[0])
+            for g in gates[1:]:
+                mat = np.kron(mat, StateTomography._get_pauli_matrix(g))
+            # Updated density matrix
+            rho += total * mat
+        return rho
+
+    @staticmethod
+    def _get_all_measurements(n_qubits: int) -> list[str]:
         """
         Returns all measurements required for a state tomography of n qubits.
         """
         # Find all measurement combinations
         measurements = list(MEASUREMENT_MAPPING.keys())
-        for _i in range(self.n_qubits - 1):
+        for _i in range(n_qubits - 1):
             measurements = [
                 g1 + g2 for g1 in measurements for g2 in MEASUREMENT_MAPPING
             ]
         return measurements
 
-    def _get_required_measurements(self) -> tuple[list, dict]:
+    @staticmethod
+    def _get_required_measurements(n_qubits: int) -> tuple[list, dict]:
         """
         Calculates reduced list of required measurements assuming that any
         measurements in the I basis can be replaced with a Z measurement.
         A dictionary which maps the full measurements to the reduced basis is
         also returned.
         """
-        mapping = {c: c.replace("I", "Z") for c in self._get_all_measurements()}
+        mapping = {
+            c: c.replace("I", "Z")
+            for c in StateTomography._get_all_measurements(n_qubits)
+        }
         req_measurements = list(set(mapping.values()))
         return req_measurements, mapping
 
-    def _get_measurement_operator(self, measurement: str) -> Circuit:
+    @staticmethod
+    def _get_measurement_operator(measurement: str) -> Circuit:
         """
         Returns the circuit required to transform between a measurement into the
         Z basis.
@@ -272,7 +285,8 @@ class StateTomography:
             raise ValueError("Provided measurement value not recognised.")
         return MEASUREMENT_MAPPING[measurement]
 
-    def _get_pauli_matrix(self, measurement: str) -> np.ndarray:
+    @staticmethod
+    def _get_pauli_matrix(measurement: str) -> np.ndarray:
         """
         Returns the pauli matrix associated with an observable.
         """
