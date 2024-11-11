f831dfb03f79bdfa9066457cb4367dfc084f8760
Harry Bromley <harry.bromley@aegiq.com>
2024-11-11 12:10:30 +0000

    Documentation across mle tomography algorithm

--- lightworks/tomography/process_tomography_mle.py
+++ lightworks/tomography/process_tomography_mle.py
@@ -54,37 +54,46 @@ class MLEProcessTomography(ProcessTomography):
 
     def process(self) -> np.ndarray:
         """
-        Desc
+        Performs process tomography with the configured elements and calculates
+        the choi matrix using maximum likelihood estimation.
+
+        Returns:
+
+            np.ndarray : The calculated choi matrix for the process.
+
         """
         all_inputs = combine_all(TOMO_INPUTS, self.n_qubits)
         results = self._run_required_experiments(all_inputs)
         nij = {}
+        # Convert counts to an expectation value
         for (in_state, meas), result in results.items():
             # Remove trivial measurement here
             if meas == ",".join("I" * self.n_qubits):
                 continue
             nij[in_state, meas] = _calculate_expectation_value(meas, result)
-
-        n_vec = []
-        for n in nij.values():
-            n_vec.append((1 + n) / 2)
-            n_vec.append((1 - n) / 2)
-        n_vec_a = np.array(n_vec) / len(nij)
-
+        # Run MLE algorithm and get choi
         mle = MLETomographyAlgorithm(self.n_qubits)
-        self._choi = mle.pgdb(n_vec_a)
+        self._choi = mle.pgdb(nij)
         return self.choi
 
 
 class MLETomographyAlgorithm:
     """
-    Desc
+    Implements the pgdB algorithm for maximum likelihood estimation from
+    https://arxiv.org/abs/1803.10062 for calculation of a physical choi matrix
+    from the tomography measurement data.
+
+    Args:
+
+        n_qubits (int) : The number of qubits used within the tomography.
+
     """
 
-    def __init__(self, n_qubits: int, stop_threshold: float = 1e-10) -> None:
+    def __init__(self, n_qubits: int) -> None:
         self.n_qubits = n_qubits
-        self.stop_threshold = stop_threshold
 
+        # Pre-calculate all required n_qubit pauli & density matrices + the
+        # inputs and measurement basis for the tomography
         self._all_rhos = combine_all(RHO_MAPPING, self.n_qubits)
         self._all_pauli = combine_all(PAULI_MAPPING, self.n_qubits)
         self._input_basis = combine_all(TOMO_INPUTS, self.n_qubits)
@@ -92,27 +101,55 @@ class MLETomographyAlgorithm:
 
         self._a_matrix = self._a_mat()
 
-    def pgdb(self, n_vec: np.ndarray, max_iter: int = 2000) -> np.ndarray:
+    def pgdb(
+        self,
+        data: dict[tuple[str, str], float],
+        max_iter: int = 1000,
+        stop_threshold: float = 1e-10,
+    ) -> np.ndarray:
         """
-        Desc
+        Runs the pgdB algorithm on the provided data set.
+
+        Args:
+
+            data (dict) : The measured tomography experiment data. The keys of
+                this dictionary should be the input/measurement basis and the
+                values should be the calculated expectation values.
+
+            max_iter (int, optional) : Sets the maximum number of iterations
+                that the algorithm can do, defaults to 1000.
+
+            stop_threshold (float, optional) : Sets the stopping threshold for
+                the gradient descent algorithm. Defaults to 1e-10.
+
+        Returns:
+
+            np.ndarray : The calculated choi matrix
+
         """
+        # Convert data to required vector format
+        n_vec = self._n_vec_from_data(data)
+
+        # Initialize choi as identity and find initial cost
         dim = 2**self.n_qubits
         choi = np.identity(dim**2, dtype=complex) / dim
-
         current_cost = self._cost(choi, n_vec)
 
+        # Define algorithm parameters
         mu = 3 / (2 * dim**2)
         gamma = 0.3
 
+        # Run gradient descent
         for _ in range(max_iter):
-            mod = (
-                self._cptp_proj(
-                    choi - 1 / mu * self._gradient(choi, n_vec),
-                    max_iter=max_iter,
-                )
-                - choi
+            # Find modification to the choi matrix
+            mod = self._cptp_proj(
+                choi - 1 / mu * self._gradient(choi, n_vec),
+                max_iter=max_iter,
             )
-            alpha = 1.0
+            mod -= choi
+
+            # Optimise alpha weighting parameter
+            alpha = 0.5
             new_cost = self._cost(choi + alpha * mod, n_vec)
             thresh_value = gamma * np.trace(
                 mod @ np.conj(self._gradient(choi.T, n_vec))
@@ -121,21 +158,39 @@ class MLETomographyAlgorithm:
                 alpha *= 0.5
                 new_cost = self._cost(choi + alpha * mod, n_vec)
 
+            # Update choi
             choi += alpha * mod
 
-            if current_cost - new_cost < 1e-12:
+            # Check cost is still improving, otherwise stop
+            if current_cost - new_cost < stop_threshold:
                 break
-
             current_cost = new_cost
-
+        # Warn if the max iterations are exceeded
         else:
             warnings.warn("Max iterations exceeded.", stacklevel=1)
 
         return choi
 
+    def _n_vec_from_data(
+        self, data: dict[tuple[str, str], float]
+    ) -> np.ndarray:
+        """
+        Converts the data from dictionary format into the required vector,
+        ensuring this is correct regardless of the data ordering of the
+        dictionary.
+        """
+        n_vec = np.zeros(2 * len(data), dtype=complex)
+        for i, in_s in enumerate(self._input_basis):
+            for j, meas in enumerate(self._meas_basis):
+                coord = 2 * (len(self._meas_basis) * i + j)
+                n = data[in_s, meas]
+                n_vec[coord : coord + 2] = [(1 + n) / 2, (1 - n) / 2]
+        return n_vec / len(data)
+
     def _a_mat(self) -> np.ndarray:
         """
-        Desc
+        Calculates the A matrix which is used for vectorisation of the pgdB
+        algorithm.
         """
         dim1 = len(self._input_basis) * len(self._meas_basis) * 2
         dim2 = 2 ** (4 * self.n_qubits)
@@ -154,39 +209,44 @@ class MLETomographyAlgorithm:
 
     def _p_vec(self, choi: np.ndarray) -> np.ndarray:
         """
-        Desc
+        Calculates the expected measurement outcomes from the provided choi
+        matrix.
         """
-        p = self._a_matrix @ vec(choi.T)
-        return p.clip(1e-6)
+        return (self._a_matrix @ vec(choi.T)).clip(1e-8)
 
     def _cost(self, choi: np.ndarray, n_vec: np.ndarray) -> np.ndarray:
         """
-        Desc
+        Computes the variation between the current choi matrix and the
+        calculated values.
         """
         return -np.real(n_vec.T @ np.log(self._p_vec(choi)))
 
     def _gradient(self, choi: np.ndarray, n_vec: np.ndarray) -> np.ndarray:
         """
-        Desc
+        Finds gradient between expected and measured expectation values.
         """
         return -unvec(np.conj(self._a_matrix.T) @ (n_vec / self._p_vec(choi)))
 
     def _cptp_proj(self, choi: np.ndarray, max_iter: int = 1000) -> np.ndarray:
         """
-        Desc
+        Performs the CPTP algorithm, ensuring the matrix is completely positive
+        and trace preserving. This uses a series of repeat TP and CP steps until
+        the choi matrix converges.
         """
         x_0 = vec(choi)
         dim = x_0.shape[0]
+        # Initialize most quantities as a zero matrix
         p_0 = np.array([0] * dim)
         q_0 = np.array([0] * dim)
         y_0 = np.array([0] * dim)
-
+        # Run for maximum number of iterations
         for _ in range(max_iter):
+            # Calculate updated quantiles
             y_k = vec(self._tp_proj(unvec(x_0 + p_0)))
             p_k = x_0 + p_0 - y_k
             x_k = vec(self._cp_proj(unvec(y_k + q_0)))
             q_k = y_k + q_0 - x_k
-
+            # Stopping condition (see paper)
             if (
                 np.linalg.norm(p_0 - p_k) ** 2
                 + np.linalg.norm(q_0 - q_k) ** 2
@@ -194,14 +254,15 @@ class MLETomographyAlgorithm:
                 + abs(2 * np.conj(q_0) @ (y_k - y_0))
             ) < 10**-4:
                 break
-
+            # Update values
             y_0, p_0, x_0, q_0 = y_k, p_k, x_k, q_k
 
         return unvec(x_k)
 
     def _cp_proj(self, choi: np.ndarray) -> np.ndarray:
         """
-        Desc
+        Performs the CP part of the projection by enforcing that the eigenvalues
+        of the matrix are all positive.
         """
         vals, vecs = np.linalg.eigh(choi)
         d = np.diag([max(v, 0) for v in vals])
@@ -209,13 +270,14 @@ class MLETomographyAlgorithm:
 
     def _tp_proj(self, choi: np.ndarray) -> np.ndarray:
         """
-        Desc
+        Performs the TP part of the projection by enforcing that the partial
+        trace of the choi matrix is equal to the identity matrix.
         """
+        # Compute partial trace
         dim = int(choi.shape[0] ** 0.5)
-
         partial_trace = choi.reshape(np.tile([dim, dim], 2))
         partial_trace = np.einsum(partial_trace, [0, 1, 2, 1])
         partial_trace = partial_trace.reshape(dim, dim)
-
-        diff = partial_trace - np.identity(dim)
-        return choi - np.kron(diff / dim, np.identity(dim))
+        # Then find the variation and subtract this from the choi matrix
+        variation = partial_trace - np.identity(dim)
+        return choi - np.kron(variation / dim, np.identity(dim))
