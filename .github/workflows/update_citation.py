77ec6d851723e141865bfc7c8efa350f86689a97
Harry Bromley <harry.bromley@aegiq.com>
2025-02-24 16:43:01 +0000

    Fixed citation file name

--- .github/workflows/update_citation.py
+++ .github/workflows/update_citation.py
@@ -11,10 +11,10 @@ date_str = (
     f"{current_date.day:02d}'\n"
 )
 
-with open("CITATION.ff", encoding="utf-8") as f:
+with open("CITATION.cff", encoding="utf-8") as f:
     all_lines = f.readlines()
 
-with open("CITATION.ff", "w", encoding="utf-8") as f:
+with open("CITATION.cff", "w", encoding="utf-8") as f:
     for line in all_lines:
         if line.startswith("date-released:"):
             f.write(date_str)
