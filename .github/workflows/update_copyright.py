01252f6dd84f8278a150dc5ef774a380b34fef65
Harry Bromley <harry.bromley@aegiq.com>
2025-07-29 11:58:18 +0100

    Python script to update copyright of all python files with current year

---
+++ .github/workflows/update_copyright.py
@@ -0,0 +1,21 @@
+import datetime
+import os
+
+current_year = f"{datetime.datetime.now(tz=datetime.UTC).year:04d}"
+
+for directory in ["lightworks", "docs", "tests"]:
+    for root, _, files in os.walk(f"{directory}"):
+        # Generate list of all python files in a directory
+        py_files = [f for f in files if f.endswith(".py")]
+        for f_name in py_files:
+            with open(root + "/" + f_name, encoding="utf-8") as f:
+                lines = f.readlines()
+            # Loop through all lines until the copyright one is found, then edit
+            # this and break the loop
+            for i, line in enumerate(lines):
+                if line.startswith("# Copyright"):
+                    lines[i] = line[:19] + current_year + line[23:]
+                    continue
+            # Write new python file
+            with open(root + "/" + f_name, "w", encoding="utf-8") as f:
+                f.writelines(lines)
